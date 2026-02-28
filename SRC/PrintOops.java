1abcb77c31251ab4d91821f3bd9748d5b7fe915d
sriya <dsriya964@gmail.com>
2026-02-28 21:57:02 +0530

    UC6 done

--- SRC/PrintOops.java
+++ SRC/PrintOops.java
@@ -1,18 +1,49 @@
-public class PrintOops {
-    public static void main(String[] args) {
-
-        String[] lines = { 
-            String.join(" ","   ****   ","   ****    ","  *******  ","   ***** "),
-            String.join(" ","  **  **  ","  **  **   ","  **    ** "," **    "),
-            String.join(" "," **    ** "," **    **  ","  **    ** ","  **    "),
-            String.join(" "," **    ** "," **    **  ","  *******  ","    *** "),
-            String.join(" "," **    ** "," **    **  ","  **       ","       **"),
-            String.join(" ","  **  **  ","  **  **   ","  **       ","       **"),
-            String.join(" ","   ****   ","   ****    ","  **       ","   ***** ")
+public class PrintOops{
+    public static String[] getO1Pattern(){
+        return new String[]{
+            "*****",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*****"
         };
+    }
+    public static String[] getO2Pattern(){
+        return new String[]{
+            "*****",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*****"
+        };
+    }
+    public static String[] getPPattern(){
+        return new String[]{
+            "*****",
+            "*   *",
+            "*****",
+            "*    ",
+            "*    "
+        };
+    }
+    public static String[] getSPattern(){
+        return new String[]{
+            "******",
+            "*     ",
+            "******",
+            "     *",
+            "******"
+        };
+    }
+    public static void main(String[] args){
+        
+        String[] O1Pattern = getO1Pattern();
+        String[] O2Pattern = getO2Pattern();
+        String[] PPattern = getPPattern();
+        String[] SPattern = getSPattern();
 
-        for (String line : lines) {
-            System.out.println(line); 
-        } 
+        for (int i=0;i<O1Pattern.length;i++){
+            System.out.println(O1Pattern[i]+" "+O2Pattern[i]+" "+PPattern[i]+" "+SPattern[i]);
+        }
     }
 } 
